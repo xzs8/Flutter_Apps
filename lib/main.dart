721b75cb85c3ca95b87ba2dd9b2cb520ac99a6d4
Khaled Baowdhan <khaled.baowdhan@gmail.com>
2025-11-28 17:12:27 +0300

    برنامج المسبحة لنظام اندرويد

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'سبحة الكترونية',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-      ),
-      home: const MyHomePage(title: 'سبحة'),
-    );
+    return MaterialApp();
   }
 }
 
@@ -29,80 +23,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter1 = 0;
-  int _counter2 = 0;
-  int _counter3 = 0;
 
-  void _incrementCounter1() {
-    setState(() {
-      _counter1++;
-    });
-  }
-
-  void _incrementCounter2() {
-    setState(() {
-      _counter2++;
-    });
-  }
-
-  void _incrementCounter3() {
-    setState(() {
-      _counter3++;
-    });
-  }
-
-  void _resetCounter() {
-    setState(() {
-      _counter1 = 0;
-      _counter2 = 0;
-      _counter3 = 0;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter1',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
 
-            ElevatedButton(
-              onPressed: _incrementCounter1,
-              child: const Text("سبحان الله"),
-            ),
-            Text(
-              '$_counter2',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-
-            ElevatedButton(
-              onPressed: _incrementCounter2,
-              child: const Text("الحمدلله"),
-            ),
-            Text(
-              '$_counter3',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: _incrementCounter3,
-              child: const Text("الله اكبر"),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: _resetCounter,
-          child: const Icon(Icons.restart_alt)
-      ),
     );
   }
 }
