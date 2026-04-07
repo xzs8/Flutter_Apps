9193825296d90c8a6cd068640d295abc3eed3866
Khaled Baowdhan <khaled.baowdhan@gmail.com>
2026-04-07 08:37:35 +0300

    .

--- lib/MyHomePage .dart
+++ lib/MyHomePage .dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'handlingExcelFiles.dart';
-
 
 class MyHomePage  extends StatefulWidget {
   const MyHomePage ({super.key});
@@ -12,11 +10,7 @@ class MyHomePage  extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage > {
 
-  List<List<String>> storData = [];
 
-  TextEditingController fileName=TextEditingController();
-
-  Excelfiles excelObj=new Excelfiles();
 
   @override
   Widget build(BuildContext context) {
@@ -29,58 +23,11 @@ class _MyHomePageState extends State<MyHomePage > {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 7),
-            TextFormField(
-              controller: fileName,
-              decoration: InputDecoration(
-                labelText: 'file Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16),
-
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async{
-                String? path = await excelObj.pickFile();
-
-                if(path != null){
-                  fileName.text = path.split('/').last;
-                  storData = await excelObj.readFile(path);
-                  setState(() {
-
-                  });
-                }
-              },
-              child: const Text('Chose an excel file'),
-            ),
-            SizedBox(height: 5),
-            ElevatedButton(onPressed: (){
-               storData = [];
-               fileName.clear();
-               setState(() {
-
-               });
-             },
-              child: Text("Clear")),
-            Expanded(
-              child: ListView.builder(
-                itemCount: storData.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(
-                      storData[index].join(" | "),
-                    ),
-                  );
-                },
-              ),
-            ),
-
+          children: [
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'MyHomePage .dart';
 
-
 void main() {
   runApp(const MyApp());
 }
